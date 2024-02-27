2202054db50d33f17f4780f91e928f334144108c
Alberto Zelaya <albertozelaya5@yahoo.com>
2024-02-26 23:07:03 -0600

    Avance Lunes 28 de feb diseno

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import banhcafeAPI from './api/banhcafeAPI';
-import logo from './assets/logo-text.png';
+import logo from './assets/Logotipo.png';
 
 
 export const App = () => {
@@ -23,14 +23,14 @@ export const App = () => {
   return (
     <>
       <div className="flex items-center justify-center h-screen">
-          <form className="uppercase text-center rounded shadow-xl p-4 m-8">
+          <form className="uppercase text-center rounded-md shadow-xl p-4 m-8 lg:p-8 pb-2">
             <img className="mx-auto w-1/2" src={logo} alt="logo" />
-            <h1 className="font-semibold text-xl tracking-wide">Tasa de Cambio</h1>
+            <h1 className="text-2xl tracking-wide py-2">Tasa de Cambio</h1>
             <h2 className="text-2xl">Dólar de EE.UU hacia Lempira Hondureño</h2>
             <hr className="h-px my-4 bg-gray-200 border-0 dark:bg-gray-700" />
-            <h3 className="tracking-wide my-2">Compra:</h3>
+            <h3 className="tracking-wide my-2 font-semibold">Compra:</h3>
             <p className='font-semibold'>{`${exchangeRate.buy}`}</p>
-            <h3 className="tracking-wide my-2">Venta:</h3>
+            <h3 className="tracking-wide my-2 font-semibold">Venta:</h3>
             <p className='font-semibold'>{`${exchangeRate.sell}`}</p>
             {/* <p className='font-semibold'>{usdSale}</p> */}
           </form>
